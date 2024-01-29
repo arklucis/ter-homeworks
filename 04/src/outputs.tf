b1dce1d53103e7b050e4fccc52c25a8db95a558a
Alekc <alekc.genom@yandex.ru>
2024-01-29 10:34:21 +0000

    fixed ter_Homeworks_04

--- 04/src/outputs.tf
+++ 04/src/outputs.tf
@@ -1,9 +1,1 @@
-/*
-output "my_vpc_id" {
-  value = module.my_vpc.vpc_id
-}
 
-output "my_subnet_id" {
-  value = module.my_vpc.subnet_id
-}
-*/
--- 04/src/vpc_module/outputs.tf
+++ 04/src/vpc_module/outputs.tf
@@ -6,3 +6,19 @@ output "vpc_id" {
 output "subnet_id" {
   value = yandex_vpc_subnet.vpc.id
 }
+
+output "vpc_name" {
+  value = var.vpc_name
+}
+
+output "zone" {
+  value = var.default_zone
+}
+
+output "cidr" {
+  value = var.default_cidr
+}
+
+output "network_id" {
+  value = yandex_vpc_network.vpc.id
+}
